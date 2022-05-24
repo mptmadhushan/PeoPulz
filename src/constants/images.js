e9ae66221a0422fe92e92554d57d62e9c2830208
mptmadhushan <tharindu.madhushan@axiatadigitallabs.com>
2022-05-24 20:40:04 +0530

    first commit

---
+++ src/constants/images.js
@@ -0,0 +1,13 @@
+const shoe = require('../assets/images/shoe.png');
+const logo = require('../assets/images/LOGO.png');
+const silicaLogo = require('../assets/images/silica_logo.jpeg');
+const nfcImage = require('../assets/images/nfcImage.png');
+const radar = require('../assets/images/radar.png');
+
+export default {
+  logo,
+  silicaLogo,
+  shoe,
+  radar,
+  nfcImage,
+};
